1300953c910ca2332701025e08f40979828879c4
xuqun.liu <xuqun.liu@thundersoft.com>
2025-06-20 10:30:28 +0800

    RTC_ID:1064323
    COMMENT:[SGM_Navigation750_Guidance]【导航】车头朝上模式下，导航显示正北模式，点击继续导航后闪现车头朝上模式后切换为正北模式15:58
    
    Root Cause:选路态时开启了全览和车标预览模式，导致强制北上
    
    Solution:发起导航时应该取消全览和车标预览
    
    PatchType:Bug
    
    PatchModule:NaviApp
    
    PatchLevel:P3
    
    Change-Id: Ia0da7faa159947ecc74f8b11e01fbca1813fa562
    UTResult: N/A

--- app/src/main/java/com/fy/navi/hmi/navi/NaviGuidanceModel.java
+++ app/src/main/java/com/fy/navi/hmi/navi/NaviGuidanceModel.java
@@ -310,6 +310,8 @@ public class NaviGuidanceModel extends BaseModel<NaviGuidanceViewModel> implemen
                 mLayerPackage.openDynamicLevel(MapType.MAIN_SCREEN_MAIN_MAP,
                         DynamicLevelMode.DYNAMIC_LEVEL_GUIDE);
             }
+            //全览之后开启导航，需要取消全览
+            mMapPackage.exitPreview(MapType.MAIN_SCREEN_MAIN_MAP);
             // 延时绘制终点扎标，减少峰值消耗
             ThreadManager.getInstance().postDelay(mFirstDrawEndPoint, NumberUtils.NUM_1000);
             // 开始三分钟查询一次终点POI信息
