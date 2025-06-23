c9a0fb6708cebddabd20f51e6b5c138b4d5ac7b7
xiwei.zhang <xiwei.zhang@thundersoft.com>
2025-06-23 17:08:11 +0800

    RTC_ID:1061889 COMMENT:弹窗层级问题 设置地图桌面后高德隐私政策弹框与桌面卡片重叠 Root Cause:层级问题 Solution:层级问题 PatchType:BugFix PatchModule:NaviApp PatchLevel:P2
    Change-Id: I1d07b0678483976fda0a75b3d4c90fb3ccf114c4

--- app/src/main/java/com/sgm/navi/hmi/navi/AuthorizationRequestDialog.java
+++ app/src/main/java/com/sgm/navi/hmi/navi/AuthorizationRequestDialog.java
@@ -1,16 +1,21 @@
 package com.sgm.navi.hmi.navi;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.sgm.navi.hmi.R;
 import com.sgm.navi.hmi.databinding.DialogAuthorizationRequestBinding;
 import com.sgm.navi.ui.dialog.BaseDialog;
+import com.android.utils.log.Logger;
 
 public class AuthorizationRequestDialog extends BaseDialog<DialogAuthorizationRequestBinding> {
 
+    private static final String TAG = AuthorizationRequestDialog.class.getSimpleName();
     private final Context mContext;
     public AuthorizationRequestDialog(final Context context) {
         super(context);
@@ -26,6 +31,16 @@ public class AuthorizationRequestDialog extends BaseDialog<DialogAuthorizationRe
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setCanceledOnTouchOutside(false);
+    }
+
+    @Override
+    public void show() {
+        Window window = getWindow();
+        if (window != null) {
+            Logger.d(TAG, "show getWindow: ", window.getAttributes().type);
+            window.setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY);
+        }
+        super.show();
     }
 
     @Override
