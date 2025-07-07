2d9fa35104087242700446343978ed6813c0b256
nan.shu <nan.shu@thundersoft.com>
2025-07-07 12:09:30 +0800

    RTC_ID:1077629 COMMENT:[SGM_Navigation750_Route]【UI】路线规划页画面终点名称所在框宽度与UI设计不符 RootCause:未适配ND样式 Solution:适配ND样式 PatchType:Bug PatchModule:NaviApp PatchLevel:P3
    Change-Id: Iff195cfa66ac430c4af4cb17293dd5a8109b4947

--- app/src_clea_ndnc/main/java/com/sgm/navi/hmi/route/RouteViewModel.java
+++ app/src_clea_ndnc/main/java/com/sgm/navi/hmi/route/RouteViewModel.java
@@ -2,11 +2,21 @@ package com.sgm.navi.hmi.route;
 
 import android.app.Application;
 
+import com.sgm.navi.hmi.BuildConfig;
+
 public class RouteViewModel extends BaseRouteViewModel {
     public RouteViewModel(final Application application) {
         super(application);
-        setMsgDialogTop(100);
-        setMsgDialogLeft(700);
+        if (BuildConfig.FLAVOR.equals("clea_local_8155")
+                || BuildConfig.FLAVOR.equals("clea_8775")) {
+            setMsgDialogTop((int) application.getResources()
+                    .getDimension(com.sgm.navi.service.R.dimen.dp_53));
+            setMsgDialogLeft((int) application.getResources()
+                    .getDimension(com.sgm.navi.service.R.dimen.dp_579));
+        }else{
+            setMsgDialogTop(100);
+            setMsgDialogLeft(700);
+        }
     }
 
     @Override
