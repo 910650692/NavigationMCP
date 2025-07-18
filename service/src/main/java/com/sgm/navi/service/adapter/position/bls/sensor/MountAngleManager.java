7879f11c2f6a10d4b3927f8684e5898706210e07
niuxiaoqi <niuxiaoqi1@thundersoft.com>
2025-07-18 12:27:51 +0800

    RTC_ID:1055332
    COMMENT:更新local8155安装角
    
    PatchType:Task
    
    PatchModule:NaviApp
    
    PatchLevel:P1
    
    Change-Id: Ic79184720d76a5924c69e931c600f2bb7368e5df
    UTResult: N/A

--- service/src/main/java/com/sgm/navi/service/adapter/position/bls/sensor/MountAngleManager.java
+++ service/src/main/java/com/sgm/navi/service/adapter/position/bls/sensor/MountAngleManager.java
@@ -19,11 +19,10 @@ public class MountAngleManager {
         // TODO: 2025/2/25 此处后期可做车型区分
         info.yaw = 0.0f;
         info.pitch = 0.0f;
+        info.roll = 0.0f;
         //TODO flavor判断需要优化，当前缺陷:Flavor名称改动后遗漏改动风险
         if (BuildConfig.FLAVOR.equals("clea_local_8155")) {
-            info.roll = 180.f;
-        } else {
-            info.roll = 0.0f;
+            info.pitch = 180.0f;
         }
         info.x = 0; //原x轴
         info.y = 1; //原y轴
