3046f2a8d680c9340dc86882a1a20bda690bec23
chenjiaxin <jiaxin.chen@thundersoft.com>
2025-07-22 19:40:32 +0800

    RTC_ID:1092903 终点停车推荐气泡显示逻辑错误
    COMMENT:终点停车推荐气泡显示逻辑错误
    
    Root Cause: 终点停车场显示逻辑错误
    
    Solution: 删除比例尺限制
    
    PatchType:Bug
    
    PatchModule:NaviApp
    
    PatchLevel:P2
    
    Change-Id: Ia860d55e76ba9bdd152a5d64455661c75711d956
    UTResult:Pass

--- service/src/main/java/com/sgm/navi/service/adapter/layer/bls/style/LayerLabelStyleAdapter.java
+++ service/src/main/java/com/sgm/navi/service/adapter/layer/bls/style/LayerLabelStyleAdapter.java
@@ -21,8 +21,6 @@ public class LayerLabelStyleAdapter extends BaseStyleAdapter {
         switch (item.getBusinessType()) {
             case BizLabelType.BizLabelTypeRoutePopSearchPoint -> {
                 Logger.d(TAG,"终点停车场图层使用自定义扎标");
-                // 设置显示比例尺范围5m~2km
-                layer.setDisplayScale(new LayerScale(12.0f, 22.0f));
                 return KEY_LABEL_END_PARK;
             }
         }
