3b63c03ce7e868d8144d7abcf0de2e01251dc408
wanglei0902 <wanglei0902@thundersoft.com>
2025-08-22 13:38:00 +0800

    RTC_ID:1103670
    COMMENT:导航中语音无法切换路线，说切换后直接切换下一首音乐
    
    Root cause:当前没有实现弹框，无法触发可见即可说
    
    Solution:添加语音多轮
    
    PatchType:Bug
    
    PatchModule:NaviApp
    
    PatchLevel:P3
    
    Change-Id: I87af7eb307371d6430a0c6afcd0f8ff3731dab94
    UTResult:Pass

--- vrbridge/src/main/java/com/sgm/navi/vrbridge/IVrBridgeConstant.java
+++ vrbridge/src/main/java/com/sgm/navi/vrbridge/IVrBridgeConstant.java
@@ -227,7 +227,7 @@ public interface IVrBridgeConstant {
 
         String NOT_SUPPORT_LIGHT_NAVI = "暂不支持切换经典导航";
         String NOT_SUPPORT_THIS_FUNCTION = "不支持此功能";
-        String NOT_SUPPORT_OPERATE = "不支持的操作类型";
+        String NOT_SUPPORT_OPERATE = "不支持的指令类型";
 
         String ALREADY_IN_NAVI = "当前已在导航中";
         String LETS_GO = "开始出发";
@@ -472,6 +472,9 @@ public interface IVrBridgeConstant {
         String UN_SUPPORT_SORT = "当前搜索结果不支持排序";
 
         String UN_SUPPORT_IN_NAVI = "导航态不支持此指令";
+
+        String ROAD_ASSIGN_DONE = "搞定";
+        String ROAD_ASSIGN_NO = "收到";
     }
 
     interface PoiPageType {
@@ -491,6 +494,7 @@ public interface IVrBridgeConstant {
     }
 
     String CHANGING_ROUTE_CONFIRM = "UPDATE_CHANGING_ROUTE";
+
     interface RouteChargeType {
         int SEARCH = 3;
         int REFRESH = 5;
@@ -498,5 +502,14 @@ public interface IVrBridgeConstant {
 
     String HIDE_OVERLAY_ACTION = "patac.intent.ACTION_BROADCAST_HIDE_ALLAPPS";
     String PKG_SUSTEMUI = "com.android.systemui";
+
+    interface MultipleRoundAction {
+        String CONFIRM = "CONFIRM";
+        String NO = "CONFIRM_NO";
+    }
+    interface RoadAssignAction {
+        String ASSIGN = "ASSIGNED_ROAD";
+        String NON_ASSIGN = "NON_ASSIGNED_ROAD";
+    }
 
 }
