fcbea337150a180f74a4126b74aba48976dbf87d
song.shi <song.shi@thundersoft.com>
2025-09-02 10:44:55 +0800

    RTC_ID:1074428
    COMMENT:问路况，该路在导航路线上，回复：仅支持导航路线上的路况查询
    
    Root Cause:获取道路数据方式会遗漏数据
    
    Solution:判断所有的link数据，不遗漏道路数据
    
    PatchType:bug
    
    PatchModule:NaviApp
    
    Change-Id: Iffb482b76736770ab89ab79891bcf1bc643c33df
    PatchLevel:P3

--- service/src/main/java/com/sgm/navi/service/define/navi/RoadName.java
+++ service/src/main/java/com/sgm/navi/service/define/navi/RoadName.java
@@ -2,17 +2,19 @@ package com.sgm.navi.service.define.navi;
 
 import java.util.HashMap;
 
+import lombok.Getter;
+
 public class RoadName {
     // 道路唯一标识
     private long mPathId;
     // String为道路名称，Integer为道路的LinkIndex，LinkIndex用于获取对应道路的Tmc状态
-    private HashMap<String, Integer> mRoadNameMap;
+    private HashMap<String, RoadLink> mRoadNameMap;
 
-    public HashMap<String, Integer> getRoadNameMap() {
+    public HashMap<String, RoadLink> getRoadNameMap() {
         return mRoadNameMap;
     }
 
-    public void setRoadNameMap(final HashMap<String, Integer> roadNameAndTmc) {
+    public void setRoadNameMap(final HashMap<String, RoadLink> roadNameAndTmc) {
         this.mRoadNameMap = roadNameAndTmc;
     }
 
@@ -22,5 +24,17 @@ public class RoadName {
 
     public void setPathId(final long pathId) {
         this.mPathId = pathId;
+    }
+
+    @Getter
+    public static class RoadLink {
+        int length;
+        int linkIndex;
+
+        public RoadLink(int length, int linkIndex) {
+            this.length = length;
+            this.linkIndex = linkIndex;
+        }
+
     }
 }
